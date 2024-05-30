ce3a612d95950589ea3badc90532c81c5d4d8775
Mari-Anne Mee <mari-anne@MariAnnesLaptop.lan>
2024-05-30 14:55:09 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using SystemNet;
+using Week16BitCoinApp;
+
+
+BitCoinRate bitcoin = GetRates()
+
+Console.WriteLine($"Current BitCoin rate is: {bitcoin.bpi.EUR.rate_float} {bitcoin.bpi.EUR.code}");
+
+static BitCoinRate GetRates()
+{
+    string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+    HttpsWebRequest request = (HttpsWebRequest)WebRequest.Create(url);
+    request.Method = "GET";
+
+    var WebResponse = request.GetResponse();
+    var webStream = WebResponse.GetResponseStream();
+
+    BitCoinRate bitcoinRate;
+
+    using (var responseReader = new StreamReader(webStream))
+    {
+        var response = responseReader.ReadToEnd();
+        bitcoinRate = JsonConvert.DeserializeObject<bitcoinRate>(response);
+    }
+
+
+    return bitcoinRate;
+}
+
+
